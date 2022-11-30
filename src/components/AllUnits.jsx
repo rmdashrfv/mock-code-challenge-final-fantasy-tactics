ef8fbfbd928ea1f71a81f26fa27511121ee5c42d
Michael Law <rmdashrfv@hey.com>
2022-11-30 11:31:55 -0500

    completed deliverable 1

--- src/components/AllUnits.jsx
+++ src/components/AllUnits.jsx
@@ -1,8 +1,19 @@
-const AllUnits = () => {
+import UnitCard from './UnitCard'
+
+const AllUnits = ({ allUnits }) => {
   // maybe some code here 🤔
   return(
     <div className="all-units">
        List of all units here ...
+       {
+         allUnits.map((unit) => {
+           return(
+             <div>
+               <UnitCard unit={unit} />
+             </div>
+           )
+         })
+       }
     </div>
   )
 }
--- src/components/UnitsPage.jsx
+++ src/components/UnitsPage.jsx
@@ -1,13 +1,25 @@
 import YourParty from "./YourParty";
 import AllUnits from "./AllUnits";
+import { useEffect, useState } from "react";
 
 //start here with your code for step one
 
 const UnitsPage = () => {
+  const [allUnits, setAllUnits] = useState([])
+  
+  useEffect(() => {
+    const request = async () => {
+      let req = await fetch("http://localhost:3000/units")
+      let res = await req.json()
+      setAllUnits(res)
+    }
+    request()
+  }, [])
+  
   return(
     <div>
       <YourParty />
-      <AllUnits />
+      <AllUnits allUnits={allUnits} />
     </div>
   )
 }
